ce83f32fd8f46b5dbe514619b6fc64266c57d560
Francisco Javier Gaviria Sierra <fgaviria@psl.com.co>
2020-10-26 05:16:55 -0500

    feat: refactor to user service
    add Dockerfile
    add travis file
    Remove feed service

--- src/controllers/v0/index.router.ts
+++ src/controllers/v0/index.router.ts
@@ -1,11 +1,9 @@
 import {Router, Request, Response} from 'express';
-import {FeedRouter} from './feed/routes/feed.router';
 import {UserRouter} from './users/routes/user.router';
 
 // eslint-disable-next-line new-cap
 const router: Router = Router();
 
-router.use('/feed', FeedRouter);
 router.use('/users', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {
